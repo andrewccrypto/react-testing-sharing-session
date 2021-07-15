abc15618c846955e5bf45a430e18d5081a6826a9
Andrew Chung <andrew.chung@crypto.com>
2021-07-15 20:48:39 +0800

    Update tests for PokemonElementTypeTag

--- components/__tests__/PokemonElementTypeTag.tsx
+++ components/__tests__/PokemonElementTypeTag.tsx
@@ -1,8 +1,16 @@
 import { render } from "@testing-library/react";
 import PokemonElementTypeTag from "@components/PokemonElementTypeTag";
-import { PokemonElementGrass } from "@types";
+import { PokemonElementBug, PokemonElementGrass } from "@types";
 
 describe("PokemonElementTypeTag", () => {
+  it("Should handle Bug element type", () => {
+    const { container } = render(
+      <PokemonElementTypeTag element={PokemonElementBug} />
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+
   it("Should handle Grass element type", () => {
     const { container } = render(
       <PokemonElementTypeTag element={PokemonElementGrass} />
